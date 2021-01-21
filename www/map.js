616e873c7c78cd73889bc9f9cd8aeb04184e1121
nesati <59739799+nesati@users.noreply.github.com>
2021-01-21 19:10:37 +0100

    fixed comparison resizing

--- www/map.js
+++ www/map.js
@@ -1,6 +1,6 @@
-var map = L.map('mapid').setView([50.0878859, 14.3873313], 15);
+const map = L.map('mapid').setView([50.0878859, 14.3873313], 15);
 map.invalidateSize()
-var photoIcon = L.icon({
+const photoIcon = L.icon({
     iconUrl: 'img/photo-icon.svg',
 
     iconSize: [30, 30], // size of the icon
@@ -17,17 +17,18 @@ fetch("markers.json").then(r => {
         r.json().then(data => {
             data.forEach(val => {
                 L.marker([val["lat"], val["lng"]], {icon: photoIcon}).addTo(map)
-                    .bindPopup(val["desc"])
+                    .bindPopup(val["title"])
                     .on('click', function (e) {
+                        compare(val["img1"], val["img2"])
                         document.getElementById('img-right').src = [val["img1"]],
-                            document.getElementById('img-left').src = [val["img2"]],
-                            document.getElementById('title-of-comparison').innerHTML = [val["desc"]],
-                            document.getElementById('img-right-year').innerHTML = [val["img1-year"]],
-                            document.getElementById('img-left-year').innerHTML = [val["img2-year"]],
-                            document.getElementById('img-left-index').href = [val["img2-href"]],
-                            document.getElementById('img-left-index').innerHTML = [val["img2-src"]],
-                            document.getElementById('img-right-index').href = [val["img1-href"]],
-                            document.getElementById('img-right-index').innerHTML = [val["img1-src"]];
+                        document.getElementById('img-left').src = [val["img2"]],
+                        document.getElementById('title-of-comparison').innerHTML = [val["desc"]],
+                        document.getElementById('img-right-year').innerHTML = [val["img1-year"]],
+                        document.getElementById('img-left-year').innerHTML = [val["img2-year"]],
+                        document.getElementById('img-left-index').href = [val["img2-href"]],
+                        document.getElementById('img-left-index').innerHTML = [val["img2-src"]],
+                        document.getElementById('img-right-index').href = [val["img1-href"]],
+                        document.getElementById('img-right-index').innerHTML = [val["img1-src"]];
                     })
             })
         });
